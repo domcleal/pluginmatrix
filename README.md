bec0118f56791aded134abfc1b19555871e817a6
Dominic Cleal <dcleal@redhat.com>
2015-03-23 19:12:33 +0000

    Initial commit
    Adds APIs for retrieving gem, RPM and Debian package versions for Foreman
    plugins.

---
+++ README.md
@@ -0,0 +1,42 @@
+# Foreman plugin matrix
+
+Shows versions of Foreman plugins in rubygems.org, our apt and yum repositories (well, our source control for them at any rate) by way of badges.
+
+## API
+
+### GET /gem/:name
+
+Examples:
+
+* `/gem/foreman-tasks`
+* `/gem/foreman-tasks?compare=~%3E0.5.0`
+
+Gets the latest version from rubygems.org, optionally matching a gem version specification (`%3E` is `>`).
+
+### GET /deb/:repo/:package/:name
+
+Examples:
+
+* `/deb/develop/ruby-foreman-tasks/foreman-tasks`
+* `/deb/1.8/ruby-foreman-tasks/foreman-tasks?compare=~%3E0.6.0`
+
+Gets the latest Debian package version for a given gem, optionally comparing against a gem version specification (`%3E` is `>`).
+
+### GET /rpm/:repo/:package/:name
+
+Examples:
+
+* `/rpm/develop/rubygem-foreman-tasks/foreman-tasks`
+* `/rpm/1.8/rubygem-foreman-tasks/foreman-tasks?compare=~%3E0.6.0`
+
+Gets the latest RPM package version for a given gem, optionally comparing against a gem version specification (`%3E` is `>`).
+
+## Copyright
+
+Copyright (c) 2015 Dominic Cleal
+
+This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
---
+++ app.rb
@@ -0,0 +1,80 @@
+require 'sinatra'
+require 'json'
+require 'octokit'
+require 'gems'
+require 'base64'
+
+def get_rubygems(name, compare = '>= 0')
+  gemcmp = Gem::Dependency.new('', compare)
+  newest = Gems.versions(name).find_all { |v| gemcmp.match?('', v['number']) }.map { |v| Gem::Version.new v['number'] }.max
+  newest ? newest.version : nil
+end
+
+def get_octokit
+  Octokit::Client.new
+end
+
+def show_badge(badge_name, gem_name, pkg_gem_version, compare)
+  colour = 'blue'
+  unless compare.nil?
+    gem_version = get_rubygems(gem_name, compare)
+    colour = (gem_version && pkg_gem_version >= gem_version) ? 'green' : 'yellow'
+  end
+  redirect "https://img.shields.io/badge/#{badge_name}-#{pkg_gem_version}-#{colour}.svg"
+end
+
+get '/gem/:name' do
+  version = if params[:compare]
+              get_rubygems(params[:name], params[:compare])
+            else
+              get_rubygems(params[:name])
+            end
+
+  if version
+    redirect "https://img.shields.io/badge/gem-#{version}-green.svg"
+  else
+    redirect 'https://img.shields.io/badge/gem-not%20found-red.svg'
+  end
+end
+
+get '/deb/:repo/:package/:file' do
+  halt 'invalid repo' unless params[:repo] =~ /\A([\d\.]+|develop)\z/
+  halt 'invalid package' unless params[:package] =~ /\A[\w-]+\z/
+  halt 'invalid filename' unless params[:file] =~ /\A[\w-]+\z/
+
+  begin
+    bundler = get_octokit.contents('theforeman/foreman-packaging', ref: "deb/#{params[:repo]}", path: "plugins/#{params[:package]}/#{params[:file]}.rb")
+  rescue Octokit::NotFound
+    redirect 'https://img.shields.io/badge/deb-missing-red.svg'
+  end
+  bundler = Base64.decode64(bundler[:content])
+
+  if bundler =~ /\Agem\s+['"]([\w-]+)['"],\s+['"]([\w\.-]+)['"]/
+    gem_name, pkg_gem_version = $1, $2
+  else
+    halt 'cannot determine gem version from packaging repo'
+  end
+
+  show_badge('deb', gem_name, pkg_gem_version, params[:compare])
+end
+
+get '/rpm/:repo/:package/:file' do
+  halt 'invalid repo' unless params[:repo] =~ /\A([\d\.]+|develop)\z/
+  halt 'invalid package' unless params[:package] =~ /\A[\w-]+\z/
+  halt 'invalid filename' unless params[:file] =~ /\A[\w-]+\z/
+
+  begin
+    files = get_octokit.contents('theforeman/foreman-packaging', ref: "rpm/#{params[:repo]}", path: params[:package])
+  rescue Octokit::NotFound
+    redirect 'https://img.shields.io/badge/rpm-missing-red.svg'
+  end
+
+  filename = files.map { |f| f['name'] }.find { |f| f.end_with? '.gem' }
+  if filename && filename =~ /\A(#{params[:file]})-([\w\.-]+)\.gem\Z/
+    gem_name, pkg_gem_version = $1, $2
+  else
+    halt 'cannot determine gem version from packaging repo'
+  end
+
+  show_badge('rpm', gem_name, pkg_gem_version, params[:compare])
+end
